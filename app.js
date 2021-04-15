22db60289f5f171686573edfb9b4adb5d97bf696
OmarBelguith <omar.belghith96@gmail.com>
2021-04-15 15:51:10 +0100

    feat: CRUD for Students

--- app.js
+++ app.js
@@ -1,32 +1,79 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-app.use(express.json())
 
-const students = [];
+app.use(express.json());
 
-app.post('/students', (req, res, next)=> {
-    students.push(req.body);
-    console.log('1st call');
-    next();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/students', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connection.once('open', function() {
+  console.log('Connected to database');
 });
-app.post('/students', (req, res, next) => {
-    console.log('2nd call');
-    res.send('Students Added');
-})
-app.get('/students', (req, res, next)=> {
-    res.json(students)
+
+
+const StudentSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    skills: [String],
+    age: Number
 });
-app.get('/students/:index', (req, res, next)=> {
-    res.json(students[req.params.index])
+const Student = mongoose.model('Student', StudentSchema);
+
+const auth = (req, res, next) => {
+    try {
+        const {id} = jwt.verify(req.get('Authorization'), 'anysecret');
+        const student = Student.findById(id);
+        req.user = student;
+        next();
+    } catch (error) {
+        res.send('Unauthorized Access')
+    }
+}
+
+app.post('/students', async (req, res)=> {
+    try {
+        req.body.password = await bcrypt.hash(req.body.password, 12);
+        await Student.create(req.body);
+        res.send('Student Created Succesfully');   
+    } catch (error) {
+        res.send('Problem with server');
+    }
+
 });
-app.delete('/students', (req, res, next) => {
-    students.length = 0;
-    res.send('All students are deleted')
+app.get('/students', auth ,async(req, res)=> {
+    res.json(await Student.find());
 });
-app.delete('/students/:index', (req, res, next) => {
-    students.splice(req.params.index, 1);
-    res.json(students)
+app.get('/students/:id', auth ,async (req, res)=> {
+    res.json(await Student.findById(req.params.id));
+});
+app.post('/login', async (req, res)=> {
+    try {
+        const student = await Student.findOne({email: req.body.email});
+        const match = await bcrypt.compare(req.body.password, student.password)
+        if(!match){
+            res.send('Wrong Password')
+        }else{
+            res.send(jwt.sign({id: student._id}, 'anysecret'))
+        }
+    } catch (error) {
+        console.error(error);
+        res.send('Wrong Email')
+    }
+
+});
+app.delete('/students', async (req, res, next) => {
+    await Student.deleteMany();
+    res.send('Deleted All Students')
+});
+app.delete('/students/:id', async (req, res, next) => {
+    await Student.findByIdAndDelete(req.params.id);
+    res.send('Student Deleted')
 })
 
 app.listen(3000, ()=>{
